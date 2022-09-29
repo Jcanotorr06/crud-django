cda2df42d0990e8c3f7f079772bb617b517cc55e
Jcanotorr06 <jcano062001@gmail.com>
2022-09-29 15:14:02 -0500

    update: add productioon URl to trusted CSRF origins

--- crudapp/settings.py
+++ crudapp/settings.py
@@ -114,6 +114,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://crud-django-production.up.railway.app'
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
